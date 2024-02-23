47bea7713b6c416b1ccfba53784b0e7fa6b49898
senalw <senaldulanjala@gmail.com>
2024-02-23 16:53:55 +0530

    test: create sample data in products table when application start-up

--- src/main.py
+++ src/main.py
@@ -1,3 +1,5 @@
+from typing import Any, Dict
+
 from fastapi import FastAPI
 from src.api.v1.routes import routers as v1_routers
 from src.core.container import Container
@@ -6,7 +8,7 @@ from src.util.singleton import singleton
 
 @singleton
 class AppCreator:
-    def __init__(self):
+    def __init__(self) -> None:
         # set app default
         self.app = FastAPI(
             title=Container.conf.project_config.name,
@@ -18,18 +20,25 @@ class AppCreator:
         # set db and container
         self.container = Container()
         self.db = self.container.db()
+
+        # BELOW SECTION ADDED FOR TESTING PURPOSE #
+        # table creation can be done using flyway
         self.db.drop_tables()
         self.db.create_tables()
 
+        # insert same data
+        self.db.insert_sample_data()
+        #######################################################
+
         # Route to serve the Swagger UI
         @self.app.get("/docs", include_in_schema=False)
-        async def custom_swagger_ui_html():
-            return app.openapi_html()
+        async def custom_swagger_ui_html() -> Any:
+            return self.app.openapi_html()
 
         # Route to serve the OpenAPI schema
         @self.app.get("/openapi.json", include_in_schema=False)
-        async def get_openapi():
-            return app.openapi()
+        async def get_openapi() -> Dict[str, Any]:
+            return self.app.openapi()
 
         self.app.include_router(v1_routers, prefix="/api/v1")
 
